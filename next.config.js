1738499665190f8d194a8bd1e1c4e45b65844177
whn <me@whn.se>
2021-06-16 22:25:40 +0200

    add redirs for old posts

---
+++ next.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source:
+          "/never-write-another-for-loop-replacing-java-8-for-loops-with-foreach-calls-using-the-stream-api-539b799172cc",
+        destination:
+          "/posts/Never-write-another-for-loop-Replacing-Java-8-for-loops-with-forEach-calls-using-the-Stream-API",
+        permanent: true,
+      },
+      {
+        source: "/installing-meteor-on-archlinux-f80a7505bac7",
+        destination: "/posts/installing-meteor-on-archlinux/",
+        permanent: true,
+      },
+      {
+        source: "/the-implicit-explicit-gap-69cc5fe8fcf5",
+        destination: "/posts/the-implicit-explicit-gap",
+        permanent: true,
+      },
+      {
+        source: "/writing-good-react-components-9923f6682d85",
+        destination: "/posts/writing-good-react-components",
+        permanent: true,
+      },
+      {
+        source: "/k-means-clustering-in-r-c24115efdc82",
+        destination: "/posts/k-means-clustering-in-r-c24115efdc82",
+        permanent: true,
+      },
+    ];
+  },
+};
