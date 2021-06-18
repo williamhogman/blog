20aa6c3b843096d3ad5c9bfbbf4d9f17fd8fc584
whn <me@whn.se>
2021-06-18 19:56:01 +0200

    add proper page to feeed dummy

--- pages/feed_dummy.tsx
+++ pages/feed_dummy.tsx
@@ -2,6 +2,10 @@ import { generateRss } from "../lib/rss";
 import { GetStaticProps } from "next";
 import fs from "fs/promises";
 
+export default function FeedDummy() {
+  return <div>Feed dummy</div>;
+}
+
 export const getStaticProps: GetStaticProps = async (context) => {
   const data = await generateRss();
   await fs.writeFile("./public/rss.xml", data);
