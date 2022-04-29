56b2eeaf1b6584b263e4f5de46635dd5b35fc366
whn <me@whn.se>
2022-04-29 22:37:30 +0200

    update build

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -1,12 +1,13 @@
-import { generateRss } from "../../lib/rss"
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { readFile } from 'fs/promises';
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function feedFunc(req: NextApiRequest, res: NextApiResponse) {
+export default async function feedFunc(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/xml; charset=utf-8");
-    res.end(await readFile(new URL('../rss.xml', import.meta.url), 'utf-8'));
+    res.end();
   } catch (e) {
     console.log(e);
     res.statusCode = 500;
