dc2168f3f8c4cf25cc8fbd0f5a5c69b2bf1038cd
William Högman Rudenmalm <me@whn.se>
2023-01-02 23:18:54 +0100

    fully port to /app

--- app/head.tsx
+++ app/head.tsx
@@ -3,5 +3,16 @@ import { NextSeo } from "next-seo";
 import { NEXT_SEO_DEFAULT } from "../next-seo.config"; // your path will vary
 
 export default async function Head({ params }: { params: { id: string } }) {
-  return <NextSeo {...NEXT_SEO_DEFAULT} useAppDir={true} />;
+  return (
+    <NextSeo
+      {...NEXT_SEO_DEFAULT}
+      openGraph={{
+        type: "website",
+        title: "Will vs Technology - William Rudenmalm's blog",
+        description:
+          "Where I, William Rudenmalm, document my quixotic struggle against technology.",
+      }}
+      useAppDir={true}
+    />
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,16 @@
-import { NextSeo } from "next-seo";
 import Link from "next/link";
 import Bubble from "../components/Bubble";
 import Layout from "../components/Layout";
 import Post from "../components/Post";
-import type { ClientSidePost } from "../lib/posts";
 import { getSortedPostsData } from "../lib/posts";
 import styles from "./index.module.css";
 
-export async function getStaticProps() {
+type Props = {};
+
+export default async function Home(props: Props) {
   const allPostsData = await getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-type Props = { allPostsData: ClientSidePost[] };
-
-export default function Home({ allPostsData }: Props) {
   return (
     <Layout home>
-      <NextSeo
-        openGraph={{
-          type: "website",
-          title: "Will vs Technology - William Rudenmalm's blog",
-          description:
-            "Where I, William Rudenmalm, document my quixotic struggle against technology.",
-        }}
-      />
       {allPostsData.map((post, index) => (
         <Bubble key={index} className={styles.postBubble}>
           <Post post={post} />
