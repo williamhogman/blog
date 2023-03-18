a5a0aaf0a5192b2c3b5a92b0aaec260711645b56
William Högman Rudenmalm <me@whn.se>
2023-03-18 22:41:45 +0100

    remove the author now in layout

--- app/(content)/posts/[id]/page.tsx
+++ app/(content)/posts/[id]/page.tsx
@@ -55,7 +55,6 @@ export default async function Post({ params: { id } }: PostProps) {
         style={{ display: "contents" }}
         dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
       />
-      <AboutTheAuthor />
     </>
   );
 }
