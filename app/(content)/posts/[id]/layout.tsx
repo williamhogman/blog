e9968f1a55b3646bc8c9131e5a8fe67ec41c420a
William Högman Rudenmalm <me@whn.se>
2023-03-26 22:26:16 +0200

    add comments section

--- app/(content)/posts/[id]/layout.tsx
+++ app/(content)/posts/[id]/layout.tsx
@@ -1,5 +1,15 @@
 import AboutTheAuthor from "../../../../components/AboutTheAuthor";
 
+const REMARK_42_SCRIPT_TAG = `
+var remark_config = {
+    host: 'https://comments.sobel.io/',
+    site_id: 'wvst',
+}
+`;
+const REMARK_42_SCRIPT = `
+!function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);
+`;
+
 export default function PostLayout({
   children, // will be a page or nested layout
 }: {
@@ -7,7 +17,18 @@ export default function PostLayout({
 }) {
   return (
     <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: REMARK_42_SCRIPT_TAG,
+        }}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: REMARK_42_SCRIPT,
+        }}
+      />
       {children}
+      <div id="remark42"></div>
       <AboutTheAuthor />
     </>
   );
